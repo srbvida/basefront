1eaa8ba2ab8111bab529ce25a827ffe8e9e2e66b
srbvida <srbvida@mail.com>
2025-01-12 12:09:27 +0100

    home page component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../services/auth.service';
+import {CardComponent} from '../../components/card/card.component';
+import {Router, RouterLink} from '@angular/router';
+import {IProduct} from '../../interfaces/IProduct';
+import {ProductService} from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CardComponent,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  productService = inject(ProductService);
+  arrProducts : IProduct[];
+  isAuthenticated: boolean = false;
+
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router) {
+    this.arrProducts = [];
+  }
+  async ngOnInit() : Promise<void> {
+    try{
+      this.arrProducts = await this.productService.getAll();
+    }catch(err) {
+      console.log('Error al conectar a la API: '+err);
+    }
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    console.log('Sesión cerrada desde el componente');
+    location.reload();
+  }
 
 }
+
