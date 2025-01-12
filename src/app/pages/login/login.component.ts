8e341b9d523e386331afe17fff0e7dd5617488a1
srbvida <srbvida@mail.com>
2025-01-12 12:01:11 +0100

    login page component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import {AuthService} from '../../services/auth.service';  // Servicio de autorizacion
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+  errorMessage: string = ''; // Variable para el mensaje de error
+
+  constructor(private authService: AuthService, private router: Router, formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+  }
+  async onSubmit(){
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log('Datos del formulario:', { username, password });
+
+      await this.authService.login(username, password);
+
+      // Verificamos si el usuario está autenticado
+      if (this.authService.isAuthenticated()){
+        console.log('Usuario autenticado, redirigiendo...');
+        this.router.navigate(['/home']);  // Redirigimos a la página de home
+      }else{
+        this.errorMessage = 'Usuario o contraseña incorrectos';
+      }
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
+
 }
+
